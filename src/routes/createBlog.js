e986d43a409c5bc7dd75a6b70dbd3d0d099a6fda
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-02 22:10:10 +0900

    記事作成画面の更新

--- src/routes/createBlog.js
+++ src/routes/createBlog.js
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function CreateBlog() {
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
   return (
     <div style={{ margin: "auto", width: "1000px" }}>
       <h1>ブログ作成画面</h1>
@@ -10,7 +13,23 @@ function CreateBlog() {
       <div>
         <Link to="/blogs">記事一覧画面</Link>
       </div>
+      <h2>タイトルを入力してください</h2>
+    <div>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        style={{ width: "500px" }}
+      />
     </div>
+    <h2>本文を入力してください</h2>
+    <div>
+      <textarea
+        value={contents}
+        onChange={(e) => setContents(e.target.value)}
+        style={{ width: "500px", height: "300px" }}
+      />
+    </div>      
+  </div>
   );
 }
 
