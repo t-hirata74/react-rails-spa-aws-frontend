a74327e2b0499881c601ec1f5f990bf44730adf9
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-02 22:51:02 +0900

    記事一覧の内容を更新

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { axiosInstance } from "../utils/axios.js";
 
 function Blogs() {
+    const [blogs, setBlogs] = useState();
+    useEffect(() => {
+      const fetch = async () => {
+        const res = await axiosInstance.get("/blogs");
+        setBlogs(res.data);
+      };
+    fetch();
+    }, []);
+
   return (
     <div style={{ margin: "auto", width: "1000px" }}>
       <h1>ブログ一覧画面</h1>
@@ -9,6 +20,16 @@ function Blogs() {
       </div>
       <div>
         <Link to="/create">記事作成画面</Link>
+      </div>
+
+      <div>
+        <ul>
+          {blogs?.map((b) => (
+            <Link to={`/blogs/${b.id}`} key={b.id}>
+              <li>{b.title}</li>
+            </Link>
+          ))}
+        </ul>
       </div>
     </div>
   );
