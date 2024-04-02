01a2dfb570a02999fa1b3f448da38bd4054b3b86
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-02 20:23:27 +0900

    TOPページにリンクを追加

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 function App() {
   return (
     <div style={{ margin: "auto", width: "1000px" }}>
       <h1>ブログアプリ</h1>
+      <div>
+        <Link to="/create">記事作成画面</Link>
+      </div>
+      <div>
+        <Link to="/blogs">記事一覧画面</Link>
+      </div>
     </div>
   );
 }
